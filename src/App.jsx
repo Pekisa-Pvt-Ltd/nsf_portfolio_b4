c1ebe8d946fcdaf5971c7d4a10d6e495792fc8e2
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-11-25 22:07:49 +0500

    React Day 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='main-section-layout'>
+      <div className='main-section-1'>Section 1</div>
+      <div className='main-section-2'>Section 2</div>
+    </div>
+  )
+}
+
+export default App
