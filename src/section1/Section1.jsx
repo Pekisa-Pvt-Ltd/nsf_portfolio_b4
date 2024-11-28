7abcd0ac391d67e0117af45fb33553de3814420f
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-11-28 22:10:45 +0500

    SECTION 1 FIXED.

--- src/section1/Section1.jsx
+++ src/section1/Section1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './section1.css'
 import profilePicture from '../images/subahn.jpg'
 import { MdEmail } from "react-icons/md";
-import { FaPhone } from "react-icons/fa";
+import { FaFacebookF, FaGithub, FaPhone, FaTwitter } from "react-icons/fa";
 import { FaBirthdayCake } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
@@ -14,24 +14,50 @@ const Section1 = () => {
         {
             img: <MdEmail />,
             title: "Email",
-            value: "tanweer@gmail.com"
+            value: "tanweer@gmail.com",
+            color: "#FFCFEF"
 
         },
         {
             img: <FaPhone />,
             title: "Phone",
-            value: "+923312345678"
+            value: "+923312345678",
+            color: "#0A97B0"
         },
         {
             img: <FaBirthdayCake />,
             title: "Date of Birth",
-            value: "5th May, 2000"
+            value: "5th May, 2000",
+            color: "#0A5EB0"
         },
         {
             img: <FaMapMarkerAlt />,
             title: "Location",
-            value: "Karachi, Pakistan"
+            value: "Karachi, Pakistan",
+            color: "#0A5EB0"
         },
+    ]
+
+
+    const socialMedia = [
+        {
+            icon: <FaFacebookF />,
+            link: "https://www.facebook.com/",
+           color: "#0A5EB0"
+
+        },
+        {
+            icon: <FaTwitter />,
+            link: "https://x.com/",
+            color: "#0A5EB0"
+
+        },
+        {
+            icon: <FaGithub />,
+            link: "https://www.github.com",
+            color: "#0A5EB0"
+
+        }
     ]
 
     return (
@@ -39,14 +65,14 @@ const Section1 = () => {
 
             <div className='section-1-first'>
 
-                <div>
+                <div className='section-1-first-pp'>
                     <img src={profilePicture} className='section-1-pp' />
                 </div>
 
-                <h1>Daniyal</h1>
+                <h1 className='main-name'>Daniyal</h1>
 
                 <div>
-                    <p>Frontend Developer</p>
+                    <p className='frontend-dev'>Frontend Developer</p>
                 </div>
 
                 <span className='section-1-first-line'></span>
@@ -56,15 +82,25 @@ const Section1 = () => {
             <div className='section-1-second'>
                 {userDetail.map((item, index) => {
                     return (
-                        <div className='section-1-data'>
-                            <img src={item.img} />
+                        <div className='section-1-data' key={index}>
+                            <span style={{ color: item.color }}>{item.img}</span>
 
 
-                            <div>
-                                <h1>{item.title}</h1>
+                            <div style={{ display: "flex", flexDirection: "column", alignItems: "start", width: "150px" }}>
+                                <p className='title'>{item.title}</p>
                                 <h4>{item.value}</h4>
                             </div>
                         </div>
+                    )
+                })}
+            </div>
+
+            <div className='section-1-last-icons'>
+                {socialMedia.map((item, index) => {
+                    return (
+                        <a href={item.link} target='_blank' key={index}>
+                            <span className='icons'>{item.icon}</span>
+                        </a>
                     )
                 })}
             </div>
